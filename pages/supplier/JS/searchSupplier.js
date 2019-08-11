9f73ab21998ce5c26a6c5c60558a15bf4c5911d9
Vineet Naran <vineetnaran@gmail.com>
2019-08-11 11:36:08 +0200

    Small edit on search supplier

--- pages/supplier/JS/searchSupplier.js
+++ pages/supplier/JS/searchSupplier.js
@@ -17,7 +17,7 @@ $(()=>{
 				formView="<form target='_blank' action='view-supplier.php' method='POST'><input type='hidden' name='ID' value='"+arr[k]["SUPPLIER_ID"]+"'>"+"<input type='hidden' name='NAME' value='"+arr[k]["NAME"]+"'>"+"<input type='hidden' name='VAT' value='"+arr[k]["VAT_NUMBER"]+"'>"+"<input type='hidden' name='PHONE' value='"+arr[k]["CONTACT_NUMBER"]+"'>"+"<input type='hidden' name='EMAIL' value='"+arr[k]["EMAIL"]+"'>"+"<button class='btn btn-icon btn-2 btn-success btn-sm' type='submit'><span class='btn-inner--icon'><i class='fas fa-user'></i></span><span class='btn-inner--text'>View</span></button>"+"</form>";
 				tableEntries+="<tr><td>"+arr[k]["SUPPLIER_ID"]+"</td><td>"+arr[k]["NAME"]+"</td><td>"+arr[k]["CONTACT_NUMBER"]+"</td><td>"+arr[k]["EMAIL"]+"</td><td>"+formView+"</td></tr>";
 			}
-			$("tBody").append(tableEntries);
+			$("#tBody").append(tableEntries);
 			
 		}
 		else
