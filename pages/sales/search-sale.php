a030df39528ac41ed14725fdc0d3b0b2752c9e2a
Kudzaishe Ranganai <u17032220@tuks.co.za>
2019-08-28 03:44:55 +0200

    Search Sale Minor Change
    removing of dummy row

--- pages/sales/search-sale.php
+++ pages/sales/search-sale.php
@@ -72,20 +72,6 @@
                 </tr>
               </thead>
               <tbody id="tBody">
-                <tr>
-                  <td>321</td>
-                  <td>25/07/2019</td>
-                  <td>Dr</td>
-                  <td>Alana</td>
-                  <td>R70.00</td>
-                  <td>
-                    <button class="btn btn-icon btn-2 btn-success btn-sm" type="button" onclick="window.location='view-sale.php'">
-                      <span class="btn-inner--icon"><i class="fas fa-eye"></i>
-                      </span>
-                      <span class="btn-inner--text">View</span>
-                    </button>
-                  </td>
-                </tr>
                 <tr id="emptySearch" style="display: none;" class="table-danger mb-3">
                   <td><b>No Sale Found</b></td>
                   <td></td>
