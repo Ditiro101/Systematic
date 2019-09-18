e121ea663ffed7e46e02d5b578afff59e3ab5745
Vineet Naran <vineetnaran@gmail.com>
2019-09-18 16:01:42 +0200

    Prep Make Delivery

--- pages/driver/JS/listDelivery.js
+++ pages/driver/JS/listDelivery.js
@@ -7,7 +7,9 @@ let buildTable=function(tmp,assArr,assPArr)
 	let saleID=assArr[tmp]["SALE_ID"];
 	let saleArr=assArr.filter(element=>element["SALE_ID"]==saleID);
 	let saleProductsArr=assPArr.filter(element=>element["SALE_ID"]==saleID);
-	let formEntry="<form action='finalise_delivery.php' method='POST'><input type='hidden' name='ass' value='"+JSON.stringify(saleArr)+"'>"+"<form type='hidden' name='assP' value='"+JSON.stringify(saleProductsArr)+"'>"+"<button class='btn btn-icon btn-2 btn-primary btn-sm' type='submit'><span class='btn-inner--icon'><i class='fas fa-truck'></i></span><span class='btn-inner--text'>Make Delivery</span></button>";
+	console.log(saleArr);
+	console.log(saleProductsArr);
+	let formEntry="<form action='finalise_delivery.php' method='POST'><input type='hidden' name='ass' value='"+JSON.stringify(saleArr)+"'>"+"<input type='hidden' name='assP' value='"+JSON.stringify(saleProductsArr)+"'>"+"<button class='btn btn-icon btn-2 btn-primary btn-sm' type='submit'><span class='btn-inner--icon'><i class='fas fa-truck'></i></span><span class='btn-inner--text'>Make Delivery</span></button>";
 	form.append(formEntry);
 	tableEntry.append(form);
 	let saleEntry=$("<td></td>").text(assArr[tmp]["SALE_ID"]);
@@ -16,7 +18,7 @@ let buildTable=function(tmp,assArr,assPArr)
 }
 $(()=>{
 	assignments=JSON.parse($("#aData").text());
-	assignmentProduct=JSON.parse($("#aData").text());
+	assignmentProduct=JSON.parse($("#apData").text());
 	for(let k=0;k<assignments.length;k++)
 	{
 		buildTable(k,assignments,assignmentProduct);
--- pages/driver/JS/selectDeliveryTruck.js
+++ pages/driver/JS/selectDeliveryTruck.js
@@ -1,10 +1,12 @@
 var assignments;
 var assignmentProducts;
 var truckID;
+var trucks=[];
 let buildTruck=function()
 {
 	for(let k=0;k<assignments.length;k++)
 	{
+		let found=trucks.includes(assignments[k]["TRUCK_ID"]);
 		if(k==0)
 		{
 			truckID=assignments[k]["TRUCK_ID"];
@@ -13,18 +15,31 @@ let buildTruck=function()
 		let wOption=$("<option></option>").addClass("classDestination");
 		// let id="d"+num;
 		// wOption.attr("id",id);
-		wOption.attr("name",assignments[k]["TRUCK_ID"]);
-		wOption.text(assignments[k]["REGISTRATION_NUMBER"]+"|"+assignments[k]["TRUCK_NAME"]+"|"+assignments[k]["CAPACITY"]+" Tonnes");
-		dW.append(wOption);	
+		if(!found)
+		{
+			trucks.push(assignments[k]["TRUCK_ID"]);
+			wOption.attr("name",assignments[k]["TRUCK_ID"]);
+			wOption.text(assignments[k]["REGISTRATION_NUMBER"]+"|"+assignments[k]["TRUCK_NAME"]+"|"+assignments[k]["CAPACITY"]+" Tonnes");
+			dW.append(wOption);	
+		}
 	}
 }
 $(()=>{
 	assignments=JSON.parse($("#mTrucks").text());
 	assignmentProducts=JSON.parse($("#pTrucks").text());
+	console.log(assignments);
+	console.log(assignmentProducts);
 	buildTruck();
 	let filterArr=assignmentProducts.filter(element=>element["TRUCK_ID"]==truckID);
 	let filterAssignmentArr=assignments.filter(element=>element["TRUCK_ID"]==truckID);
-	$("#a1").val(JSON.stringify(filterArr));
-	$("#a2").val(JSON.stringify(filterAssignmentArr));
-
+	$("#a1").val(JSON.stringify(filterAssignmentArr));
+	$("#a2").val(JSON.stringify(filterArr));
+	$("#truckSelect").on('change',function(e){
+		e.preventDefault();
+		truckID=$(this).children(":selected").attr("name");
+		filterArr=assignmentProducts.filter(element=>element["TRUCK_ID"]==truckID);
+		filterAssignmentArr=assignments.filter(element=>element["TRUCK_ID"]==truckID);
+		$("#a1").val(JSON.stringify(filterAssignmentArr));
+		$("#a2").val(JSON.stringify(filterArr));
+	});
 });
