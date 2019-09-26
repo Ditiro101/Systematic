754ad583fdb42f369d966542a768acb38dd4aa8f
Afonos Tshepo <u17079463@tuks.co.za>
2019-09-26 16:14:01 +0200

    Revert "Merge branch 'feature-stock-Dt' into feature-Tshepo"
    This reverts commit 290e3f3784009607ff028033730556c31f25ede7.

--- pages/employee/JS/addEmployee-Ajax.js
+++ pages/employee/JS/addEmployee-Ajax.js
@@ -1,3 +1,52 @@
+var regex=/^\d{3}\d{3}\d{4}$/;
+var idRegex=/^\d{3}\d{3}\d{7}$/;
+var emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+let CheckValid = function(valArr)
+{
+    if(valArr["contact"].length!=10)
+    {
+        $("#MMessage").text("Contact Number must be 10 digits long.");
+        $("#btnClose").attr("data-dismiss","modal");
+        $("#displayModal").modal("show");
+        return false;
+    }
+    else if (valArr["IDPASS"].length!=13)
+    {
+        $("#MMessage").text("ID number must be 13 digits long.");
+        $("#btnClose").attr("data-dismiss","modal");
+        $("#displayModal").modal("show");
+        return false;
+    }
+    else if(regex.test(valArr["contact"])!=true)
+    {
+        $("#MMessage").text("Contact Number must only contain digits");
+        $("#btnClose").attr("data-dismiss","modal");
+        $("#displayModal").modal("show");
+        return false;
+    }
+    else if(idRegex.test(valArr["IDPASS"])!=true)
+    {
+        $("#MMessage").text("ID Number must only contain digits");
+        $("#btnClose").attr("data-dismiss","modal");
+        $("#displayModal").modal("show");
+        return false;
+    }
+    else if(emailRegex.test(valArr["email"])!=true)
+    {
+        $("#MMessage").text("Email is not valid");
+        $("#btnClose").attr("data-dismiss","modal");
+        $("#displayModal").modal("show");
+        return false;
+
+    }
+    else
+    {
+        return true;
+    }
+    
+}
+
 let getVals = function()
 {
     let name=$("#employeeName").val().trim();
@@ -103,76 +152,113 @@ $(document).ready(function()
     $("#picToUpload").on("submit",function(e)
     {//use ID of the form
         e.preventDefault();
-        let arr=getVals();
-        let form=new FormData();
-        let pics=$("#fileUpload").get(0).files[0];
-        //$("#fileUpload").prop('files')[0]
-        form.append("file",pics);
-        form.append("name",arr["name"]);
-        form.append("surname",arr["surname"]);
-        form.append("title",arr["title"]);
-        form.append("email",arr["email"]);
-        form.append("contact",arr["contact"]);
-        form.append("employeeType",arr["employeeType"]);
-        form.append("IDPASS",arr["IDPASS"]);
-        form.append("address",arr["address"]);
-        form.append("suburb",arr["suburb"]);
-        form.append("city",arr["city"]);
-        form.append("zip",arr["zip"]);
-        form.append("status",arr["status"]);
-        console.log(pics);
-        $.ajax({
-            url:'PHPcode/addEmployee-SQL.php',
-            type:'POST',
-            data: form,
-            processData: false,
-            contentType: false,
-            cache: false,
-        })
-        .done(data=>{
-            console.log(data);
-            let confirmation = data.trim();
-            if(confirmation== "success")
+        let mainform=$("#picToUpload");
+        mainform.validate();
+        if(mainform.valid()===false)
+        {
+            e.stopPropagation();
+        }
+        else
+        {
+            let arr=getVals();
+            if(CheckValid(arr)!=true)
             {
-                $("#modal-title-default").text("Success!");
-                $("#modal-body").text("Employee added successfully");
-                $("#btnClose").attr("onclick","window.location='../../employee.php'");
-                $("#displayModal").modal("show");
-            }
-            else if(confirmation == "Employee Exists!")
-            {
-                $("#modal-title-default").text("Error!");
-                $("#modal-body").text("Employee Exists! , press close and try again");
-               
-                $("#displayModal").modal("show");
-            }
-            else if(confirmation == "City found suburb added but address not added.")
-            {
-                $("#modal-title-default").text("Error!");
-                $("#modal-body").text("City found suburb added but address not added.");
-                $("#displayModal").modal("show");
-            }
-            else if(confirmation == "error in saving employee pic or generated employee tag")
-            {
-                $("#modal-title-default").text("Error!");
-                $("#modal-body").text("error in saving employee pic or generated employee tag");
-                $("#displayModal").modal("show");
-            }
-            else if(confirmation == "Couldnt get ID of employee details")
-            {
-                $("#modal-title-default").text("Error!");
-                $("#modal-body").text("Couldnt get ID of employee details");
-                $("#displayModal").modal("show");
-            }
+                e.stopPropagation();
+            } 
             else
             {
-                
-                $("#modal-title-default").text("Error!");
-                $("#modal-body").text("Couldnt insert details");
-                $("#displayModal").modal("show");
+                let form=new FormData();
+                let pics=$("#fileUpload").get(0).files[0];
+                //$("#fileUpload").prop('files')[0]
+                form.append("file",pics);
+                form.append("name",arr["name"]);
+                form.append("surname",arr["surname"]);
+                form.append("title",arr["title"]);
+                form.append("email",arr["email"]);
+                form.append("contact",arr["contact"]);
+                form.append("employeeType",arr["employeeType"]);
+                form.append("IDPASS",arr["IDPASS"]);
+                form.append("address",arr["address"]);
+                form.append("suburb",arr["suburb"]);
+                form.append("city",arr["city"]);
+                form.append("zip",arr["zip"]);
+                form.append("status",arr["status"]);
+                console.log(pics);
+                $.ajax({
+                    url:'PHPcode/addEmployee-SQL.php',
+                    type:'POST',
+                    data: form,
+                    processData: false,
+                    contentType: false,
+                    cache: false,
+                })
+                .done(data=>{
+                    console.log(data);
+                    let confirmation = data.trim();
+                    if(confirmation.includes("success"))
+                    {
+                        let id = confirmation.split(",");
+                        let employeeID = parseInt(id[0]);
+                        console.log(id[0]);
+                        $("#modal-title-default").text("Success!");
+                        $("#MMessage").text("Employee added successfully");
+                        //$("#btnClose").attr("onclick","window.location='../../employee.php'");
+                        $("#displayModal").modal("show");
+
+
+                         $("#btnClose").click(function(e) {
+
+                                    e.preventDefault();
+                                   
+                                    window.location=`PHPcode/showGeneratedQRCode.php?employeeID=${employeeID}`;
+                                });
+                    }
+                    else if(confirmation.includes("Employee Exists"))
+                    {
+                        $("#modal-title-default").text("Error!");
+                        $("#MMessage").text("Employee Exists! , press close and try again");
+                       
+                        $("#displayModal").modal("show");
+                    }
+                    else if(confirmation == "City found suburb added but address not added.")
+                    {
+                        $("#modal-title-default").text("Error!");
+                        $("#MMessage").text("City found suburb added but address not added.");
+                        $("#displayModal").modal("show");
+                    }
+                    else if(confirmation == "error in saving employee pic or generated employee tag")
+                    {
+                        $("#modal-title-default").text("Error!");
+                        $("#MMessage").text("error in saving employee pic or generated employee tag , generate employee tag or upload picture in mainatain");
+                        $("#displayModal").modal("show");
+
+                        
+                    }
+                    else if(confirmation == "Couldnt get ID of employee details")
+                    {
+                        $("#modal-title-default").text("Error!");
+                        $("#MMessage").text("Couldnt get ID of employee details , generate employee tag or upload picture in mainatain");
+                        $("#displayModal").modal("show");
+                    }
+                    else if(confirmation.includes("There was an error within the picture upload"))
+                    {
+                        $("#modal-title-default").text("Error!");
+                        $("#MMessage").text("Incorrect picture size or format , generate employee tag or upload picture in mainatain");
+                        $("#displayModal").modal("show");
+                    }
+                    else
+                    {
+                        
+                        $("#modal-title-default").text("Error!");
+                        $("#MMessage").text(confirmation);
+                        $("#displayModal").modal("show");
+
+                        
+                    }
+                  
+                });
             }
-          
-        });
+        }
     });
     
 
